97a9985b0cdb873308866cc9bb5eae1d50eeeff8
MashoodCk <mashoodck77@gmail.com>
2025-02-03 21:17:25 +0530

    pattern added

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -5,29 +5,18 @@ public class pattern {
 
         // patter2(10);
 
-        // square(10);
+        square(10);
 
-        pattern3(10);
     }
 
+    static void square(int x) {
 
-    static void pattern3(int row){
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x; j++) {
 
-        for(int i = 0;i < row ;i++){
-            for(int j = )
-        }
-    }
-
-
-    static void square(int x){
-
-        for(int i = 1;i <= x;i++){
-            for(int j = 1;j <= x;j++){
-
-                if(j == 1 || j == x || i == 1 || i == x){
+                if (j == 1 || j == x || i == 1 || i == x) {
                     System.out.print("* ");
-                }
-                else{
+                } else {
                     System.out.print("  ");
                 }
             }
@@ -35,22 +24,20 @@ public class pattern {
         }
     }
 
+    static void patter2(int x) {
 
-    static void patter2(int x){
-
-        for(int i = 1; i < x;i++){
-            for(int j = x ;j >= i;j--){
+        for (int i = 1; i < x; i++) {
+            for (int j = x; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
+    static void pattern1(int x) {
 
-    static void pattern1(int x){
-
-        for(int i = 1;i < x;i++){
-            for(int j = 0;j < i;j++){
+        for (int i = 1; i < x; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println(" ");
