f35e96818737186edc622faceed8b76d94ca1e8a
MashoodCk <mashoodck77@gmail.com>
2025-02-03 21:18:08 +0530

    sample added

---
+++ constructor/sample.java
@@ -0,0 +1,15 @@
+// package constructor;
+
+public class sample {
+    public static void main(String[] args) {
+        System.out.println("constructor");
+        sample s = new sample();
+    }
+
+    //constructor
+    sample(){
+        System.out.println("this is sampel construcotr");
+    }
+}
+
+
