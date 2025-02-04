e5e9e495a69c8c3fb3d550d67a2a62bac747500e
MashoodCk <mashoodck77@gmail.com>
2025-02-04 11:34:47 +0530

    stuff added

---
+++ stuff/Hard.java
@@ -0,0 +1,7 @@
+
+class simple {
+    public static void main(String[] args) {
+        System.out.println("file name is different from the class name");
+        System.out.println("the class should not be public then only");
+    }
+}
