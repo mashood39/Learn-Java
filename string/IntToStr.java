f9642bc7d1ad33b13e4c13cdca18b8dcb7710fda
MashoodCk <mashoodck77@gmail.com>
2025-02-11 13:21:37 +0530

    java

---
+++ string/IntToStr.java
@@ -0,0 +1,17 @@
+public class IntToStr {
+    public static void main(String[] args) {
+        System.out.println("Integer to string");
+
+        int num = 123;
+
+        String num1 = String.valueOf(num);
+        System.out.println(num1  + 2);
+
+        String num2 = "3";
+        System.out.println(num2 + 1);
+
+        String str = num + "";
+        System.out.println(str);
+
+    }
+}
--- string/strToInt.java
+++ string/strToInt.java
@@ -1,5 +1,15 @@
 public class strToInt {
     public static void main(String[] args) {
-        System.out.println("helo");
-    }
+        System.out.println("String to integer");
+
+        String num = "124";
+
+        int num2 = Integer.parseInt(num);
+
+        System.out.println(num2 + 2);
+
+        int num3 = Integer.valueOf(num);
+
+        System.out.println(num3);
+    }           
 }
