4ea604df01aaf50ce8464c378092647be5a6d453
MashoodCk <mashoodck77@gmail.com>
2025-02-11 10:18:51 +0530

    string

---
+++ string/str.java
@@ -0,0 +1,16 @@
+class str {
+    public static void main(String[] args) {
+        System.out.println("String concatenate in java");
+
+        String str1 = "mohamed";
+        String str2 = "mashood";
+
+        String res = str1 + " " + str2;
+        String res2 = str1.concat(str2);
+
+        System.out.println(res);
+        System.out.println(res2);
+
+
+    }
+}
